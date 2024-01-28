4f0538c1eb20b7a35401775681fd6755a6c6da0c
Ali Eyvazi <ali.eyvazi1868@gmail.com>
2024-01-28 20:49:56 +0330

    exceptions refactored in check-host file

--- check-host.py
+++ check-host.py
@@ -16,24 +16,24 @@ def send_message(message):
     bot.send_message(chat_id=CHAT_ID, text=message)
 
 def checkTcp():
-  url = f"""https://check-host.net/check-tcp?host={Hostname}:{Port}&node=ir5.node.check-host.net&node=ir6.node.check-host.net&node=ir3.node.check-host.net&node=ir1.node.check-host.net"""
+  url = f"https://check-host.net/check-tcp?host={Hostname}:{Port}&node=ir5.node.check-host.net
+  &node=ir6.node.check-host.net&node=ir3.node.check-host.net&node=ir1.node.check-host.net"
   payload = {}
-  headers = {
-    'Accept': 'application/json'
-  }
+  headers = {'Accept': 'application/json'}
 
   try:
     response = requests.get(url, headers=headers, data=payload)
+    json_response = json.loads(response.text)
+    request_id=json_response["request_id"]
+    return request_id
+
   except requests.exceptions.Timeout:
-      exit
+      return
   except requests.exceptions.TooManyRedirects:
-      exit
+      return
   except requests.exceptions.RequestException as e:
       raise SystemExit(e)
-  json_response = json.loads(response.text)
-  request_id=json_response["request_id"]
 
-  return request_id
 
 def checkResult(request_id):
   url = f"https://check-host.net/check-result/{request_id}"
@@ -45,33 +45,22 @@ def checkResult(request_id):
   try:
     response = requests.get(url, headers=headers, data=payload)
   except requests.exceptions.Timeout:
-      exit
+      return
   except requests.exceptions.TooManyRedirects:
-      exit
+      return
   except requests.exceptions.RequestException as e:
       raise SystemExit(e)
   
   json_response = json.loads(response.text)
-  try:
-    res1 = json_response["ir1.node.check-host.net"][0]["time"]
-    send_message(f"ir1 {res1}")
-  except:
-      print(json_response["ir1.node.check-host.net"])
-  try:
-    res3 = json_response["ir3.node.check-host.net"][0]["time"]
-    send_message(f"ir3 {res3}")
-  except:
-      print(json_response["ir3.node.check-host.net"])
-  try:
-    res5 = json_response["ir5.node.check-host.net"][0]["time"]
-    send_message(f"ir5 {res5}")
-  except:
-      print(json_response["ir5.node.check-host.net"])
-  try:
-    res6 = json_response["ir6.node.check-host.net"][0]["time"]
-    send_message(f"ir6 {res6}")
-  except:
-      print(json_response["ir6.node.check-host.net"])
+  keys = ["ir1.node.check-host.net", "ir3.node.check-host.net", "ir5.node.check-host.net", "ir6.node.check-host.net",]
+  for key in keys:
+    try:
+      res = json_response[key][0]["time"]
+      send_message(f"{res}")
+      
+    except as e:
+      print(f"failed to get response due to: {e} for {json_response[key]} key")
+
 
 while True:
   checkResult(checkTcp())
